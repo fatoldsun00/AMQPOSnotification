2c051f51bf0d475a83db4bc26c3507af0a9c6119
Jeremy PAREZ <Jeremy@Edisac.local>
2020-10-19 11:00:11 +0200

    test autoupdate

--- main.js
+++ main.js
@@ -20,25 +20,25 @@ autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info'
 log.info('App starting...')
 autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
+  console.log('Checking for update...');
 })
 autoUpdater.on('update-available', (info) => {
-  sendStatusToWindow('Update available.');
+  console.log('Update available.');
 })
 autoUpdater.on('update-not-available', (info) => {
-  sendStatusToWindow('Update not available.');
+  console.log('Update not available.');
 })
 autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
+  console.log('Error in auto-updater. ' + err);
 })
 autoUpdater.on('download-progress', (progressObj) => {
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
+  console.log(log_message);
 })
 autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
+  console.log('Update downloaded');
 });
 
 app.on('ready', function()  {
